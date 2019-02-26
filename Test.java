756d2f62b2c3c100637c4ff302c91a337cdc84ff
AnoxiaYuYu <47996850+AnoxiaYuYu@users.noreply.github.com>
2019-02-26 15:22:58 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,21 @@
+/*
+public class Test{
+	public static void main(String[]args){
+	System.out.print("大家好");
+}
+}
+public class Test{
+	public static void main(String[]args){
+	System.out.print("你好")
+}
+}
+public class Test{
+	public static void main(String[]args){
+	System.out.print("");
+}
+}*/
+public class Test{
+	public static void main (String[]args){
+
+}
+}
